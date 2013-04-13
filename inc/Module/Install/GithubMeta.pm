f14d8cfa4cb2268dd0bfa62e0abfb5652c0cb6af
Chris Williams <chris@bingosnet.co.uk>
2013-04-13 16:06:46 +0000

    Import of BINGOS/metabase-relayd-0.30 from CPAN.
    gitpan-cpan-distribution: metabase-relayd
    gitpan-cpan-version:      0.30
    gitpan-cpan-path:         BINGOS/metabase-relayd-0.30.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- inc/Module/Install/GithubMeta.pm
+++ inc/Module/Install/GithubMeta.pm
@@ -7,26 +7,21 @@ use Cwd;
 use base qw(Module::Install::Base);
 use vars qw($VERSION);
 
-$VERSION = '0.14';
+$VERSION = '0.22';
 
 sub githubmeta {
   my $self = shift;
   return unless $Module::Install::AUTHOR;
   return unless _under_git();
   return unless $self->can_run('git');
-  return unless my ($git_url) = `git remote show -n origin` =~ /URL: (.*)$/m;
+  my $remote = shift || 'origin';
+  return unless my ($git_url) = `git remote show -n $remote` =~ /URL: (.*)$/m;
   return unless $git_url =~ /github\.com/; # Not a Github repository
   my $http_url = $git_url;
   $git_url =~ s![\w\-]+\@([^:]+):!git://$1/!;
   $http_url =~ s![\w\-]+\@([^:]+):!https://$1/!;
   $http_url =~ s!\.git$!/tree!;
-  $self->repository(
-      {
-          type => 'git',
-          url  => $git_url,
-          web  => $http_url,
-      },
-  );
+  $self->repository( $git_url );
   $self->homepage( $http_url ) unless $self->homepage();
   return 1;
 }
@@ -53,4 +48,4 @@ sub _under_git {
 'Github';
 __END__
 
-#line 114
+#line 111
